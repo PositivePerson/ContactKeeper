261fb5d8b5b90cba64f5c539b4ee1011e04518c3
Bartek <WonderfulLifeVision@gmail.com>
2020-10-28 17:38:56 +0100

    Updates

---
+++ precache-manifest.1a0f7fe787206a67e3d5a193ad3aeb0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c69f4e8cdc6744c2c7f066e6b6fde1e",
+    "url": "/ContactKeeper%20/index.html"
+  },
+  {
+    "revision": "3e64830a58cd2ab24f8d",
+    "url": "/ContactKeeper%20/static/css/main.620fe750.chunk.css"
+  },
+  {
+    "revision": "4365715db8d0b40fabef",
+    "url": "/ContactKeeper%20/static/js/2.ed618c0b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ContactKeeper%20/static/js/2.ed618c0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e64830a58cd2ab24f8d",
+    "url": "/ContactKeeper%20/static/js/main.d20d437e.chunk.js"
+  },
+  {
+    "revision": "148cdb9b2358a302f2b3",
+    "url": "/ContactKeeper%20/static/js/runtime-main.23a2c084.js"
+  }
+]);
